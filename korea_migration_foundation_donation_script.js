8718d1266020bbdc686c70ab866b28c65a609072
4x493 <4x493x1@gmail.com>
2023-09-10 01:07:04 +0900

    all done except signiture

--- korea_migration_foundation_donation_script.js
+++ korea_migration_foundation_donation_script.js
@@ -124,6 +124,72 @@ $("#reset_button").on("click", function(){
 
 
 $("#submit_button").on("click", function(){
-    $("#name").setAttribute("name", "이");
+    if(!$("#agreement1").is(":checked") || !$("#agreement2").is(":checked")) {
+        alert("결제동의정보를 확인해주세요.");
+        return;
+    }
+
+    var name = $("input[name*='name']").val();
+    var phone_num1 = $("input[name*='phone_num1']").val();
+    var phone_num2 = $("input[name*='phone_num2']").val();
+    var phone_num3 = $("input[name*='phone_num3']").val();
+    var birth_year = $("input[name*='birth_year']").val();
+    var birth_month = $("input[name*='birth_month']").val();
+    var birth_day = $("input[name*='birth_day']").val();
+    var payment_type = $("input[name*='payment_type']").val();
+    var payment_info = "";
+    if($("#one_time_payment").hasClass("active")) {
+        payment_info = "일회성 결제";
+    }
+    else {
+        payment_info = "정기결제";
+    }
+    var payment_date = $("#payment_date option:selected").text();
+    var payment_method = "";
+    if($("#payment_method").hasClass("active")) {
+        payment_method = "자동이체";
+    }
+    var bank = $("#bank option:selected").text();
+    var account_owner = $("input[name*='account_owner']").val();
+    var account_num = $("input[name*='account_num']").val();
+    var payment_ammount = "";
+    if($("#2k_won").hasClass("active")) {
+        payment_ammount = "2000원";
+    }
+    if($("#10k_won").hasClass("active")) {
+        payment_ammount = "10,000원";
+    }
+    if($("#50k_won").hasClass("active")) {
+        payment_ammount = "50,000원";
+    }
+    if(!$("#2k_won").hasClass("active") && !$("#10k_won").hasClass("active") && !$("#50k_won").hasClass("active")){
+        payment_ammount = $("input[name*='custom_won']").val();
+    }
+    var digital_signiture;
+
+    $("input[name*='name']").remove();
+    $("input[name*='phone_num1']").remove();
+    $("input[name*='phone_num2']").remove();
+    $("input[name*='phone_num3']").remove();
+    $("input[name*='birth_year']").remove();
+    $("input[name*='birth_month']").remove();
+    $("input[name*='birth_day']").remove();
+    $("input[name*='payment_type']").remove();
+    $("input[name*='account_owner']").remove();
+    $("input[name*='account_num']").remove();
+    $("input[name*='custom_won']").remove();
+
+    $("form").append("<input name='이름' style='display: none;' value='" + name + "'>");
+    $("form").append("<input name='전화번호' style='display: none;' value='" + phone_num1 + " " + phone_num2 + " " + phone_num3 + "'>");
+    $("form").append("<input name='생년월일' style='display: none;' value='" + birth_year + "년 " + birth_month + "월 " + birth_day + "일'>");
+    $("form").append("<input name='납부유형' style='display: none;' value='" + payment_type + "'>");
+    $("form").append("<input name='결제정보' style='display: none;' value='" + payment_info + "'>");
+    if(payment_info == "periodic_payment"){ $("form").append("<input name='정기 결제일' style='display: none;' value='" + payment_date + "'>"); };
+    $("form").append("<input name='결제수단' style='display: none;' value='" + payment_method + "'>");
+    $("form").append("<input name='은행' style='display: none;' value='" + bank + "'>");
+    $("form").append("<input name='예금주명' style='display: none;' value='" + account_owner + "'>");
+    $("form").append("<input name='계좌번호' style='display: none;' value='" + account_num + "'>");
+    $("form").append("<input name='결제금액' style='display: none;' value='" + payment_ammount + "'>");
+
     $("form").submit();
 });
