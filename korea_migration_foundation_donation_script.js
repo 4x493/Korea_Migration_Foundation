164d918b03cf4b8b9029fe18f51eb3de1ab8b4c7
4x493 <4x493x1@gmail.com>
2023-09-10 14:40:52 +0900

    Update korea_migration_foundation_donation_script.js

--- korea_migration_foundation_donation_script.js
+++ korea_migration_foundation_donation_script.js
@@ -125,23 +125,23 @@ $("#reset_button").on("click", function(){
 
 $("#submit_button").on("click", function(){
     if(!$("#agreement1").is(":checked") || !$("#agreement2").is(":checked")) {
-        alert("결제동의정보를 확인해주세요.");
+        alert("결제동의정보에 모두 동의해주세요.");
         return;
     }
 
-    var name = $("input[name*='name']").val();
-    var phone_num1 = $("input[name*='phone_num1']").val();
-    var phone_num2 = $("input[name*='phone_num2']").val();
-    var phone_num3 = $("input[name*='phone_num3']").val();
-    var birth_year = $("input[name*='birth_year']").val();
-    var birth_month = $("input[name*='birth_month']").val();
-    var birth_day = $("input[name*='birth_day']").val();
-    var payment_type = $("input[name*='payment_type']").val();
+    var name = ""; name = $("input[name*='name']").val();
+    var phone_num1 = ""; phone_num1 = $("input[name*='phone_num1']").val();
+    var phone_num2 = ""; phone_num2 = $("input[name*='phone_num2']").val();
+    var phone_num3 = ""; phone_num3 = $("input[name*='phone_num3']").val();
+    var birth_year = ""; birth_year = $("input[name*='birth_year']").val();
+    var birth_month = ""; birth_month = $("input[name*='birth_month']").val();
+    var birth_day = ""; birth_day = $("input[name*='birth_day']").val();
+    var payment_type = ""; payment_type = $("input[name*='payment_type']").val();
     var payment_info = "";
     if($("#one_time_payment").hasClass("active")) {
         payment_info = "일회성 결제";
     }
-    else {
+    else if($("#periodic_payment").hasClass("active")) {
         payment_info = "정기결제";
     }
     var payment_date = $("#payment_date option:selected").text();
@@ -150,8 +150,8 @@ $("#submit_button").on("click", function(){
         payment_method = "자동이체";
     }
     var bank = $("#bank option:selected").text();
-    var account_owner = $("input[name*='account_owner']").val();
-    var account_num = $("input[name*='account_num']").val();
+    var account_owner = ""; account_owner = $("input[name*='account_owner']").val();
+    var account_num = ""; account_num = $("input[name*='account_num']").val();
     var payment_ammount = "";
     if($("#2k_won").hasClass("active")) {
         payment_ammount = "2000원";
@@ -166,6 +166,13 @@ $("#submit_button").on("click", function(){
         payment_ammount = $("input[name*='custom_won']").val();
     }
     var dataURL = canvas.toDataURL("image/png");
+
+    if( name == "" || phone_num1 == "" || phone_num2 == "" || phone_num3 == "" || birth_year == "" || birth_month == "" ||
+        birth_day == "" || payment_type == "" || payment_info == "" || (payment_info == "정기결제" && payment_date == "날짜를 선택해주세요") ||
+        payment_method == "" || bank == "은행을 선택해주세요" || account_owner == "" || account_num == "" || payment_ammount == "") {
+        alert("모든 문항을 완료하였는지 확인해주세요.");
+        return;
+    }
 
     $("input[name*='name']").remove();
     $("input[name*='phone_num1']").remove();
